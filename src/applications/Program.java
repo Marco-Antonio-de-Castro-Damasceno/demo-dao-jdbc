2efff69a61f8f77804339cf07962df0ae5bc8391
Marco-Antonio-de-Castro-Damasceno <damascenomarcoantonio2019@gmail.com>
2026-01-29 15:45:30 -0300

    Reusing instantiation

--- src/applications/Program.java
+++ src/applications/Program.java
@@ -4,13 +4,13 @@ import java.util.Date;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		System.out.println("teste numero 1: find by Id");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 	}
